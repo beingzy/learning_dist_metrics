bbd66d45cee9fe87a1b0415eea6c2283f49c2479
Yi Zhang <beingzy@gmail.com>
2015-06-04 07:06:12 -0400

    update function documentation, format code in PEP-8 sytle

--- dist_metrics.py
+++ dist_metrics.py
@@ -3,67 +3,129 @@ Collection of Distance Metrics
 """
 import numpy as np
 import scipy as sp
-import pandas
+import pandas as pd
 import itertools as it
+from numpy import sqrt
+from scipy.spatial.distance import euclidean
 
 
-def weighted_euclidean(x, y, w = None):
+
+def weighted_euclidean(x, y, w=None):
     """return weigthed eculidean distance of (x, y) given weights (w)
-    
+
     distance = sqrt( sum( w[i]*(x[i] - y[i])^2 ) )
     """
-    from numpy import sqrt
-    from scipy.spatial.distance import euclidean
-
     if len(x) != len(y):
-    	print "ERROR: length(x) is different from length(y)!"
+        print "ERROR: length(x) is different from length(y)!"
 
     if w is None:
-    	w = [1] * len(x)
+        w = [1] * len(x)
 
     x = [sqrt(i_w) * i_x for i_w, i_x in zip(w, x)]
     y = [sqrt(i_w) * i_y for i_w, i_y in zip(w, y)]
     return euclidean(x, y)
 
-def pairwise_dist_wrapper(pair, data, w):
-	"""Return distance of two points by referring to data by 
-	   index specified by pair
-	"""
-	if isinstance(data, pandas.DataFrame):
-		data = data.as_matrix()
-	a = data[pair[0], :]
-	b = data[pair[1], :]
-	w = w
-	return weighted_euclidean(a, b, w)
 
-def all_pairwise_dist(pair_list, data, w):
-	"""Calculate the pair-wise distance of data points
-	   specified by pair_list 
+def pairwise_dist_wrapper(pair, data, weights=None):
+    """ Return distance of two points (rows in matrix-like data), provided
+        with a pair of row numbers.
 
-	   Parameters:
-	   -----------
-	   pair_list: {vector-like}, list of id pair_list
-	   data: {matrix-like}, store the data points information
-	   w: {vector-like, numeric), weights vector have same number of elements 
-                         as data's number of columns
+        Parameters:
+        -----------
+        * pair: {list, integer}, a vector of two integers, denoting the row
+            numbers
+        * data: {matrix-like}, matrix stores observations
+        * weights: {vector-like}, a weights vector for weighting euclidean
+            distance
 
-       Example:
-       --------
-       > all_pairwise_list(pair_list, data)
-	"""
-	dist = []
-	for p in pair_list:
-		dist.append(pairwise_dist_wrapper(p, data, w))
-	return dist
+        Returns:
+        -------
+        res: {float}, a weighted euclidean distancce between two data points
 
-def sum_grouped_dist(pair_list, data, w):
-	"""Return the sum of distance
-	"""
-	return sum(all_pairwise_dist(pair_list, data, w))
+        Examples:
+        --------
+        p = (0, 10)
+        w = [1] * data.shape[1]
+        dist = pairwise_dist_wrapper(pair, data, w)
+    """
+    if isinstance(data, pd.DataFrame):
+        data = data.as_matrix()
 
-def squared_sum_grouped_dist(pair_list, data, w):
-	""" Return the sum of squared distance
-	"""
-	dist = all_pairwise_dist(pair_list, data, w)
-	dist_squared = [ i * i for i in dist]
-	return sum(dist_squared)
+    if weights is None:
+        weights = [1] * data.shape[1]
+
+    a = data[pair[0], :]
+    b = data[pair[1], :]
+    return weighted_euclidean(a, b, weights)
+
+
+def all_pairwise_dist(pair_list, data, weights=None):
+    """ Return a series of weighted euclidean distances of the pairs of
+        data points specified by pair_list
+
+        Parameters:
+        ----------
+        * pair_list: {list, list}, a list of pairs of integers
+        * data: {matrix-like}, matrix stores observations
+        * weights: {vector-like}, a weights vector for weighting euclidean
+            distance
+
+        Returns:
+        --------
+        dist: {vector-like, float} a series of ditances
+
+        Examples:
+        --------
+        p = [[0, 1], [0, 4], [3, 4]]
+        dist = all_pairwise_dist(p, data)
+    """
+    dist = []
+    for i in pair_list:
+        dist.append(pairwise_dist_wrapper(i, data, weights))
+    return dist
+
+
+def sum_grouped_dist(pair_list, data, weights=None):
+    """ Return the sum of distance
+
+        Parameters:
+        ----------
+        * pair_list: {list, list}, a list of pairs of integers
+        * data: {matrix-like}, matrix stores observations
+        * weights: {vector-like}, a weights vector for weighting euclidean
+            distance
+
+        Returns:
+        --------
+        dist: {vector-like, float} a series of ditances
+
+        Examples:
+        --------
+        p = [[0, 1], [0, 4], [3, 4]]
+        sum_dist = sum_grouped_dist(p, data)
+    """
+    return sum(all_pairwise_dist(pair_list, data, weights))
+
+
+def squared_sum_grouped_dist(pair_list, data, weights=None):
+    """ Return the sum of squared distance
+
+        Parameters:
+        ----------
+        * pair_list: {list, list}, a list of pairs of integers
+        * data: {matrix-like}, matrix stores observations
+        * weights: {vector-like}, a weights vector for weighting euclidean
+            distance
+
+        Returns:
+        --------
+        dist: {vector-like, float} a series of ditances
+
+        Examples:
+        --------
+        p = [[0, 1], [0, 4], [3, 4]]
+        sum_dist = squared_sum_grouped_dist(p, data)
+    """
+    dist = all_pairwise_dist(pair_list, data, weights)
+    dist_squared = [d * d for d in dist]
+    return sum(dist_squared)
--- ldm.py
+++ ldm.py
@@ -5,9 +5,9 @@ Author: Yi Zhang <beingzy@gmail.com>
 Create Date: Feb/04/2015
 """
 import time
-import numpy as np 
-import pandas as pd 
-import scipy as sp 
+import numpy
+import pandas
+import scipy
 
 from itertools import combinations
 from scipy.optimize import minimize
@@ -20,16 +20,15 @@ from dist_metrics import all_pairwise_dist
 from dist_metrics import sum_grouped_dist
 from dist_metrics import squared_sum_grouped_dist
 
+
 class LDM(object):
+
     """Learning Distance Metrics (LDM)
 
     An aglorithm learning distance metrics in a supervsied
     context to tighten spreads of points known similar and
     amplify separation of points considered different in
-    a transformed space. 
-
-    Parameters:
-    -----------
+    a transformed space.
 
     Attributes:
     -----------
@@ -41,43 +40,45 @@ class LDM(object):
         over its couterpart of tarnsformed points known different
     """
 
-    def __init__(self, dist_func = None, report_excution_time= True, is_debug = False):
+    VERSION = "0.2"
+
+    def __init__(self, dist_func=None, report_excution_time=True,
+                 is_debug=False):
         self._transform_matrix = np.array([])
         self._ratio = 1
         self._report_excution_time = report_excution_time
         self._is_debug = is_debug
-        pass 
 
-    def fit(self, X, S, D = None):
+    def fit(self, X, S, D=None):
         """Fit the model with X and given S and D
 
         Parameters:
         ----------
         X: {matrix-like, np.array}, shape (n_sample, n_features)
             Training data, where n_samples is the number of n_samples
-            and n_features is the number of features 
+            and n_features is the number of features
         S: {vector-like, list} a list of tuples which define a pair of
-                  data points known as similiar 
+                  data points known as similiar
         D: {vector-like, list} a list of tuples which define a pair of
                   data points known as different
 
         Returns:
         --------
         _trans_vec: {matrix-like, np.array}, shape(n_features, n_features)
-               A transformation matrix (A) 
+               A transformation matrix (A)
         _ratio: float
         """
         self._fit(X, S, D)
-        return self 
+        return self
 
-    def fit_transform(self, X, S, D = None):
-        """Fit the model with X, S, D and conduct transformation on X
+    def fit_transform(self, X, S, D=None):
+        """ Fit the model with X, S, D and conduct transformation on X
 
         Parameters:
         -----------
         X: {matrix-like, np.array}, shape (n_sample, n_features)
             Training data, where n_samples is the number of n_samples
-            and n_features is the number of features 
+            and n_features is the number of features
 
         Returns:
         --------
@@ -88,42 +89,45 @@ class LDM(object):
         X_new = self.transform(X)
         return X_new
 
-    def _fit(self, X, S, D = None):
-        """Fit the model with given information: X, S, D
+    def _fit(self, X, S, D=None):
+        """ Fit the model with given information: X, S, D
 
+            Fit the learning distance metrics: (1) if only S is given,
+            all pairs of items in X but not in S are considered as in D;
+            (2) if both S and D given, items in X but neither in S nor in D
+            will be removed from fitting process.
 
-        Fit the learning distance metrics: (1) if only S is given,
-        all pairs of items in X but not in S are considered as in D;
-        (2) if both S and D given, items in X but neither in S nor in D
-        will be removed from fitting process.
+            Parameters:
+            ----------
+            X: {matrix-like, np.array}, shape (n_sample, n_features) matrix of
+                observations with 1st column keeping observation ID
+            S: {vector-like, list} a list of tuples which define a pair of data
+                points known as similiar
+            D: {vector-like, list} a list of tuples which define a pair of data
+                points known as different
 
-        Parameters:
-        ----------
-        X: {matrix-like, np.array}, shape (n_sample, n_features)
-            Training data, where n_samples is the number of samples
-            and n_features is the number of features 
-        S: {vector-like, list} a list of tuples which define a pair of
-                  data points known as similiar 
-        D: {vector-like, list} a list of tuples which define a pair of
-                  data points known as different
-
-        Returns:
-        --------
-        _trans_vec: {matrix-like, np.array}, shape(n_features, n_features)
-               A transformation matrix (A) 
-        _ratio: float
+            Returns:
+            --------
+            _trans_vec: {matrix-like, np.array}, shape(n_features, n_features)
+                A transformation matrix (A)
+            _ratio: float
         """
+        if isinstance(X, pandas.DataFrame):
+            X.as_matrix()
+
+        ids = X[:, 0].astype(int)
+        X = X[:, 1:]
         n_sample, n_features = X.shape
 
-        bnds = [(0, None)] * n_features # boundaries
-        init = [1] * n_features # initial weights
+        bnds = [(0, None)] * n_features  # boundaries
+        init = [1] * n_features  # initial weights
 
         if D is None:
-            all_pairs = [i for i in combinations(range(n_sample), 2)]
+            all_pairs = [p for p in combinations(ids, 2)]
             D = get_exclusive_pairs(all_pairs, S)
         else:
             covered_items = get_unique_items(S, D)
-            X = np.delete(X, covered_items, 0)
+            X = numpy.delete(X, covered_items, 0)
 
         def objective_func(w):
             a = squared_sum_grouped_dist(S, X, w) * 1.0
@@ -140,17 +144,18 @@ class LDM(object):
                 print "Examples of D: %s" % D, len(D)
                 print "Examples of X: %s" % X, X.shape
 
-
         start_time = time.time()
-        fitted = minimize(objective_func, init, method="L-BFGS-B", bounds = bnds)
+        fitted = minimize(objective_func, init, method="L-BFGS-B", bounds=bnds)
         duration = time.time() - start_time
 
         if self._report_excution_time:
             print("--- %s seconds ---" % duration)
 
+        w = self._transform_matrix
         self._transform_matrix = vec_normalized(fitted['x'])
-        self._ratio = fitted['fun'] / objective_func(init) # optimized value vs. value of initial setting
-        self._dist_func = lambda x, y: weighted_euclidean(x, y, w = self._transform_matrix)
+        # optimized value vs. value of initial setting
+        self._ratio = fitted['fun'] / objective_func(init)
+        self._dist_func = lambda x, y: weighted_euclidean(x, y, w)
 
         return (self._transform_matrix, self._ratio)
 
@@ -161,7 +166,7 @@ class LDM(object):
         -----------
         X: {matrix-like, np.array}, shape (n_sample, n_features)
             Training data, where n_samples is the number of n_samples
-            and n_features is the number of features 
+            and n_features is the number of features
 
         Returns:
         --------
@@ -171,9 +176,9 @@ class LDM(object):
         n_sample, n_features = X.shape
         trans_matrix = self._transform_matrix
         if not len(trans_matrix) == n_features:
-            raise ValueError('Transformation matrix is not compatiable with X!')
-        X_new = self._transform_matrix * X 
-
+            raise ValueError('Transformation matrix is not \
+                compatiable with X!')
+        X_new = self._transform_matrix * X
         return X_new
 
     def get_transform_matrix(self):
@@ -182,40 +187,39 @@ class LDM(object):
         Returns:
         -------
         _trans_vec: {matrix-like, np.array}, shape(n_features, n_features)
-               A transformation matrix (A) 
+               A transformation matrix (A)
         """
         return self._transform_matrix
 
     def fitted_dist_func(self, x, y):
-        """Returned the distance functions used in fitting model 
+        """ Returned the distance functions used in fitting model
 
-        Returns:
-        --------
-        func: {function} a function accept (x1, x2, *arg)
-        """     
-        if not self._transform_matrix is None:
+            Returns:
+            --------
+            func: {function} a function accept (x1, x2, *arg)
+        """
+        if self._transform_matrix is not None:
             w = self._transform_matrix
-            g = lambda x, y: weighted_euclidean(x = x, y = y, w = w)
+            g = lambda x, y: weighted_euclidean(x, y, w)
         return g(x, y)
 
     def get_ratio(self):
-        """The ratio of aggregate metrics of similiar points 
+        """The ratio of aggregate metrics of similiar points
            over the couterparts of different points
         """
         return self._ratio
 
 
-
 def get_exclusive_pairs(target_pairs, reference_pairs):
     """ Remove from target_paris the item (pairs) which
         has matches in reference_pairs.
-        
+
         Parameters:
         -----------
         target_pairs: {list}, [(1, 2), (1, 3), ...]
         reference_pairs: {list}, [(2, 1), (10, 11)]
 
-        Returns:  
+        Returns:
         -------
     """
     res_pairs = target_pairs[:]
@@ -226,6 +230,7 @@ def get_exclusive_pairs(target_pairs, reference_pairs):
                 break
     return res_pairs
 
+
 def get_unique_items(x_pairs, y_pairs):
     """Return all item mentioned either by x_pairs
        or y_pairs.
@@ -233,14 +238,15 @@ def get_unique_items(x_pairs, y_pairs):
     x_pairs.extend(y_pairs)
     res = []
     for a, b in x_pairs:
-        if not a in res:
+        if a not in res:
             res.append(a)
-        if not b in res:
+        if b not in res:
             res.append(b)
     return res
 
+
 def vec_normalized(x, digits=2):
-    """ Noramlize a vector to ensure that the sum of 
+    """ Noramlize a vector to ensure that the sum of
         elements of the vector equals to one
     """
     x_sum = sum(x) * 1.0
